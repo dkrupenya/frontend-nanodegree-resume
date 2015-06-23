0d7c9242beb5aca802844be04e7df1e07e245476
Dmitry <dkrupenya@gmail.com>
2015-06-23 23:20:48 +0300

    test javascript functions

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -4,3 +4,6 @@ var role = "Web Developer";
 $("#header").prepend(HTMLheaderRole.replace("%data%", role));
 $("#header").prepend(HTMLheaderName.replace("%data%", name));
 
+function upperCaseFirstLetter(str) {
+    return str.slice(0,1).toUpperCase() + str.slice(1);
+}
